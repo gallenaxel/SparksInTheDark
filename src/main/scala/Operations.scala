c2ed7b6af5047c3702c2f8c2960bca928832e7cd
Johannes Graner <johannes.graner@gmail.com>
2022-03-24 11:13:36 +0100

    Remove old implementation

--- src/main/scala/Operations.scala
+++ src/main/scala/Operations.scala
@@ -124,51 +124,6 @@ object LeafMapOperations {
     val (operatedLeaves, operatedVals) = operated.filter{ case (node, _) => !ancestors.contains(node) }.unzip
     LeafMap(Truncation(operatedLeaves), operatedVals)
   }
-
-
-  // def mrpOperate[A:ClassTag](leafMap1: LeafMap[A], leafMap2: LeafMap[A], op: (A, A) => A): LeafMap[A] = {
-  //   val t1 = leafMap1.truncation
-  //   val t2 = leafMap2.truncation
-  //   val t1nodes = t1.leaves
-  //   val t2nodes = t2.leaves
-  //   val commonNodes = t1nodes intersect t2nodes
-  //   val t1only = Truncation(t1.leaves diff t2.leaves)
-  //   val t2only = Truncation(t2.leaves diff t1.leaves)
-  //   val t1missing = t1only.leaves.flatMap((node: NodeLabel) => t2only.viewSubtree(node, 0)).toVector.distinct // missing from t1
-  //   val t2missing = t2only.leaves.flatMap((node: NodeLabel) => t1only.viewSubtree(node, 0)).toVector.distinct // missing from t2
-
-  //   val unionedTree = (t1missing union commonNodes.toVector union t2missing).toSet
-
-  //   val ancestorsInT1 = t1only.leaves diff t2missing
-  //   val ancestorsInT2 = t2only.leaves diff t1missing
-
-  //   def getUnionMap(
-  //     unionedTree: Set[NodeLabel], 
-  //     leafMap: Map[NodeLabel, A], 
-  //     missingNodes: Vector[NodeLabel], 
-  //     ancestorsInMissing: Vector[NodeLabel]
-  //   ): Map[NodeLabel, A] = {
-  //     var i = 0
-  //     val ancestorsOfMissing = missingNodes.tail.scanLeft(ancestorsInMissing.head)(
-  //       (anc, node) => 
-  //         if (isAncestorOf(anc, node)) 
-  //           anc 
-  //         else {
-  //           i += 1
-  //           ancestorsInMissing(i)
-  //         })
-
-  //     val missingVals = ancestorsOfMissing.map(leafMap(_))
-  //     (leafMap ++ missingNodes.zip(missingVals)).filterKeys(unionedTree.contains(_))
-  //   }
-   
-  //   val updatedMap1 = getUnionMap(unionedTree, leafMap1.toMap, t1missing, ancestorsInT1)
-  //   val updatedMap2 = getUnionMap(unionedTree, leafMap2.toMap, t2missing, ancestorsInT2)
-
-  //   val operatedLeafMap = (updatedMap1.toVector ++ updatedMap2.toVector).groupBy(_._1).mapValues(_.map(_._2).reduce(op))
-    
-  //   fromNodeLabelMap(operatedLeafMap)
-  // }
 }
 
 case class DensityHistogram(tree: SpatialTree, densityMap: LeafMap[(Probability, Volume)]) {
